c4f823d00fcdcd5b0ce68864c2d26ff3c8dde0f6
krushi-moliya <143687318+krushi-moliya@users.noreply.github.com>
2023-10-27 15:40:11 +0530

    15. Create a multidimensional array to store information about movies, including title, director, and release year for at least two movies. Display the information for one of the movies.

---
+++ Array/script_15.php
@@ -0,0 +1,7 @@
+<?php
+$movies=array(array("title"=>"Barbie","director"=>"Greta Gerwing","Year"=>"2023"),
+	          array("title"=>"Oppenheimer","director"=>" Christopher Nolan ","Year"=>"2023")
+			  );
+	print_r($movies[1]);
+?>
+
