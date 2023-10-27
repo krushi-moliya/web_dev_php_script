4f4e5c7c23109fc83f1b0935f318f85f2d4dd0b4
krushi-moliya <143687318+krushi-moliya@users.noreply.github.com>
2023-10-27 14:47:46 +0530

    Script_38 Write a PHP script to use the array_shift() function to remove an element from the beginning of an array and the array_unshift() function to add elements at the beginning.

---
+++ Task_3/script_38.php
@@ -0,0 +1,10 @@
+<?php
+$color=array("Red","Pink","Green","Orange");
+print_r($color);
+	echo "<br>";
+array_shift($color);
+print_r($color);
+	echo "<br>";
+array_unshift($color,"Black");
+print_r($color);
+?>
