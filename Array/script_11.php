f258b9eb74b720776d7fb72aa7e85ba53c349518
krushi-moliya <143687318+krushi-moliya@users.noreply.github.com>
2023-10-27 15:13:57 +0530

    11. Create two arrays, one with fruits and another with vegetables. Merge them into a single array.

---
+++ Array/script_11.php
@@ -0,0 +1,10 @@
+<?php
+$fruits=array("Apple","Mango","Orange");
+	print_r($fruits);
+	echo "<br>";
+$vegetables=array("Tomato","Potato","Carrot");
+	print_r($vegetables);
+	echo "<br>";
+$merge=array_merge($fruits,$vegetables);
+	print_r($merge);
+?>
